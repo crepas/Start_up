bd4a7badc429ed359f836e126d555d6a8ad734b0
parkj <soomj99@gmail.com>
2025-04-06 00:13:53 +0900

    엽엽 Front 4/4

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,12 @@ import 'splash.dart';  // 경로 수정
 import 'login.dart';   // 경로 수정
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';  // 수정
 import 'KakaoLogin.dart'; // KakaoLoginButton이 정의된 파일
-
+import 'ADListView.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();  // 추가
   KakaoSdk.init(nativeAppKey: '4d02a171ef1f4a73e9fd405e022dc3b2');
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(RestaurantCard()); // runapp() 에 실행 기킬 화면 넣으면 됨
 }
 
 class MyApp extends StatelessWidget {
